8a5a1e8cff389141b0c187d6da669dd8201dcaca
drowsell <drowsell@sfu.ca>
2022-04-22 13:31:03 -0700

    Initial Files

---
+++ Server.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "Server.h"
+
+struct Server server_constructor(int domain, int service, int protocol, unsigned long interface, int port, int backlog, void (*launch)(struct Server * server))
+{
+    struct Server server;
+    {
+        /* data */
+    };
+
+    server.domain = domain;
+    server.service = service;
+    server.protocol = protocol;
+    server.interface = interface;
+    server.port = port;
+    server.backlog = backlog;
+
+    server.address.sin_family = domain;
+    server.address.sin_port = htons(port);
+    server.address.sin_addr.s_addr = htonl(interface);
+
+    server.socket = socket(domain, service, protocol);
+
+    if (server.socket == 0) 
+    {
+        perror("Failed to connect socket...\n");
+        exit(1);
+    }
+
+    if (bind(server.socket, (struct sockaddr *) &server.address, sizeof(server.address)) < 0) 
+    {
+        perror("Failed to bind socket...\n");
+        exit(1);
+    }
+
+    if (listen(server.socket, server.backlog))
+    {
+        perror("Failed to start listening...\n");
+        exit(1);
+    }
+
+    server.launch = launch;
+
+    return server;
+}
