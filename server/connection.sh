c6556a93e823ef32720e32788de168b82b03aced
Dylan Rowsell <rowselldylan@gmail.com>
2022-04-26 23:26:39 +0100

    Database insert function

--- server/connection.sh
+++ server/connection.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-# Pings telus to check if the wifi is connected to the internet 
-# The > /dev/null 2>&1 just suppresses the output - it can be removed
-ping -c 1 telus.com > /dev/null 2>&1
-rc=$?
+while true
+do
+	day=`date +"%Y-%m-%d"`
+	time=`date +"%H:%M:%S"`
+	# Pings telus to check if the wifi is connected to the internet 
+	# The > /dev/null 2>&1 just suppresses the output - it can be removed
+	ping -c 1 telus.com > /dev/null 2>&1
+	rc=$?
 
-# Checks if the ping was successful
-if [[ $rc -eq 0 ]] ; then
-	echo "UP"
-else
-	echo "DOWN"
-fi
-
+	# Checks if the ping was successful
+	if [[ $rc -eq 0 ]] ; then
+		echo "${day}"
+		echo "${time}"
+		echo "UP"
+	else
+		echo "${now}"
+		echo "DOWN"
+	fi
+sleep 60
+done
